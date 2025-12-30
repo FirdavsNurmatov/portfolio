bb3f56b0df2dc87c060b4487452492a992820e2a
FirdavsNurmatov <nurmatovfirdavs96@gmail.com>
2025-12-30 16:04:43 +0500

    ready to use

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // React app domenini yozing
+    origin: 'http://localhost:3000', // React app domenini yozing
     credentials: true,
   });
   app.use(cookieParser());
